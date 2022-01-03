5c4cd04c8731a2329ff97e902102d4b8cace52e7
Anıl Çetin <75864563+llnallna@users.noreply.github.com>
2022-01-03 14:41:53 +0300

    Assignment 1 done

---
+++ NorthwindBupa.Dal/Abstract/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindBupa.Dal.Abstract
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        bool beginTransaction();
+
+        bool rollBackTransaction();
+
+        int saveChanges();
+    }
+}
---
+++ NorthwindBupa.Dal/Concrete/EntityFramework/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using NorthwindBupa.Dal.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindBupa.Dal.Concreate.EntityFramework.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public bool beginTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool rollBackTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int saveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
