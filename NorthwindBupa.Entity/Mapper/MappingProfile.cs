3cfd6c29df49701feb53551b1dfa6678b4095a18
Anll Cetinn <anil_ctn@hotmail.com>
2022-01-03 06:36:29 +0300

    Patika - Bupa&Acıbadem Sigorta Bootcamp'i 1.Odevi

---
+++ NorthwindBupa.Entity/Mapper/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using NorthwindBupa.Entity.Dto;
+using NorthwindBupa.Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindBupa.Entity.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Customer, DtoCustomer>().ReverseMap();
+            CreateMap<Order, DtoOrder>().ReverseMap();
+        }
+
+    }
+}
---
+++ NorthwindBupa.Interface/IGenericService.cs
@@ -0,0 +1,28 @@
+﻿using NorthwindBupa.Entity.IBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindBupa.Interface
+{
+    public interface IGenericService<T, TDto> where T : IEntityBase where TDto : IDtoBase
+    {
+        List<TDto> GetAll();
+        List<TDto> GetAll(Expression<Func<T, bool>> expression);
+
+        TDto Find(int id);
+
+        IQueryable<T> GetIQeryable();
+
+        TDto Add(TDto item);
+
+        Task<TDto> AddAsync(TDto item);
+        bool DeleteById(int id);
+        Task<bool> DeleteByIdAsync(int id);
+        bool Delete(TDto item);
+    }
+   
+}
